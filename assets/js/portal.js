14b2778838f0d2fe27518ba4cf5d5f60cab10f5e
astta19 <143641908+astta19@users.noreply.github.com>
2026-02-28 21:29:54 -0300

    Update portal.js

--- assets/js/portal.js
+++ assets/js/portal.js
@@ -2,7 +2,7 @@
 const SUPABASE_URL         = process.env.SUPABASE_URL;
 const SUPABASE_SERVICE_KEY = process.env.SUPABASE_SERVICE_KEY;
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
